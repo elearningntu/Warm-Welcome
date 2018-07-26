5684668ad6bd8838de765b0ae5bf676690b3225f
elearningntu <41746386+elearningntu@users.noreply.github.com>
2018-07-26 15:10:23 +0100

    Delete removePadding.js

--- removePadding.js
+++
@@ -1,6 +0,0 @@
-// Removing the padding and margin from the header div with EMAS logo
-
-let headerDiv = document.querySelector('.daylight .d2l-widget');
-headerDiv.style.marginBottom = "0";
-headerDiv.style.paddingBottom = "0";
-headerDiv.style.borderBottom = "0";
